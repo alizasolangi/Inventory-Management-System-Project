d8396a2e35a6c212221d06dcb79de880cc212fa8
Aliza SOLANGI <alizasolangi67@gmail.com>
2025-04-25 17:24:18 +0500

    Add files via upload

---
+++ OpenPdf.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Common;
+
+import dao.InventoryUtils;
+import java.io.File;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aliza SOLANGI
+ */
+public class OpenPdf {
+
+    public static void openById(String id) {
+
+        try {
+            if ((new File(InventoryUtils.billPath + id + ".pdf")).exists()) {
+                Process p = Runtime
+                        .getRuntime()
+                        .exec("rundll32 url.dll,FileProtocolHandler " + InventoryUtils.billPath + id + ".pdf");
+            } else {
+                JOptionPane.showMessageDialog(null, "File is not Exists");
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+
+    }
+}
